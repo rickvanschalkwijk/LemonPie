1ef53ef1e2d2c52db0f61fa17dda4e041b8df9cc
Rick van Schalkwijk <rickvschalkwijk@gmail.com>
2013-04-12 18:26:58 +0200

    Added TableGateway pattern

--- module/Bug/src/Bug/Model/Bug.php
+++ module/Bug/src/Bug/Model/Bug.php
@@ -17,6 +17,9 @@ class Bug {
 	public $summery;
 	public $files;
 	
+	/*
+	 * 
+	 */
 	public function exchangeArray($data) {
 		$this->id				=	(isset($data['id'])) ? $data['id'] : null;
 		$this->name				=	(isset($data['name'])) ? $data['name'] : null;
@@ -32,6 +35,202 @@ class Bug {
 		$this->operatingSystem	=	(isset($data['operatingSystem'])) ? $data['operatingSystem'] : null;
 		$this->summery			=	(isset($data['summery'])) ? $data['summery'] : null;
 		$this->files			=	(isset($data['files'])) ? $data['files'] : null;
+	}
+	
+	/**
+	 * @return the $id
+	 */
+	public function getId() {
+		return $this->id;
+	}
+	
+	/**
+	 * @return the $name
+	 */
+	public function getName() {
+		return $this->name;
+	}
+	
+	/**
+	 * @return the $description
+	 */
+	public function getDescription() {
+		return $this->description;
+	}
+	
+	/**
+	 * @return the $category
+	 */
+	public function getCategory() {
+		return $this->category;
+	}
+	
+	/**
+	 * @return the $priority
+	 */
+	public function getPriority() {
+		return $this->priority;
+	}
+	
+	/**
+	 * @return the $status
+	 */
+	public function getStatus() {
+		return $this->status;
+	}
+	
+	/**
+	 * @return the $created
+	 */
+	public function getCreated() {
+		return $this->created;
+	}
+	
+	/**
+	 * @return the $createdBy
+	 */
+	public function getCreatedBy() {
+		return $this->createdBy;
+	}
+	
+	/**
+	 * @return the $changed
+	 */
+	public function getChanged() {
+		return $this->changed;
+	}
+	
+	/**
+	 * @return the $changedBy
+	 */
+	public function getChangedBy() {
+		return $this->changedBy;
+	}
+	
+	/**
+	 * @return the $assignedTo
+	 */
+	public function getAssignedTo() {
+		return $this->assignedTo;
+	}
+	
+	/**
+	 * @return the $operatingSystem
+	 */
+	public function getOperatingSystem() {
+		return $this->operatingSystem;
+	}
+	
+	/**
+	 * @return the $summery
+	 */
+	public function getSummery() {
+		return $this->summery;
+	}
+	
+	/**
+	 * @return the $files
+	 */
+	public function getFiles() {
+		return $this->files;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $id
+	 */
+	public function setId($id) {
+		$this->id = $id;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $name
+	 */
+	public function setName($name) {
+		$this->name = $name;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $description
+	 */
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $category
+	 */
+	public function setCategory($category) {
+		$this->category = $category;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $priority
+	 */
+	public function setPriority($priority) {
+		$this->priority = $priority;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $status
+	 */
+	public function setStatus($status) {
+		$this->status = $status;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $created
+	 */
+	public function setCreated($created) {
+		$this->created = $created;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $createdBy
+	 */
+	public function setCreatedBy($createdBy) {
+		$this->createdBy = $createdBy;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $changed
+	 */
+	public function setChanged($changed) {
+		$this->changed = $changed;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $changedBy
+	 */
+	public function setChangedBy($changedBy) {
+		$this->changedBy = $changedBy;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $assignedTo
+	 */
+	public function setAssignedTo($assignedTo) {
+		$this->assignedTo = $assignedTo;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $operatingSystem
+	 */
+	public function setOperatingSystem($operatingSystem) {
+		$this->operatingSystem = $operatingSystem;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $summery
+	 */
+	public function setSummery($summery) {
+		$this->summery = $summery;
+	}
+	
+	/**
+	 * @param Ambigous <NULL, unknown> $files
+	 */
+	public function setFiles($files) {
+		$this->files = $files;
 	}
 	
 }
---
+++ module/Bug/src/Bug/Model/BugTable.php
@@ -0,0 +1,60 @@
+<?php
+namespace Bug\Model;
+
+use Zend\Db\TableGateway\TableGateway;
+
+class BugTable {
+	protected $tableGateway;
+	
+	public function __construct(TableGateway $tableGateway) {
+		$this->tableGateway = $tableGateway;
+	}
+	
+	public function fetchAll() {
+		$resultSet = $this->tableGateway->select ();
+		return $resultSet;
+	}
+	
+	public function getBug($id) {
+		$id = ( int ) $id;
+		$rowset = $this->tableGateway->select ( array ('id' => $id ) );
+		$row = $rowset->current ();
+		if (! $row) {
+			throw new Exception ( "Could not find bug with id " . $id );
+		}
+		return $row;
+	}
+	
+	public function saveBug(Bug $bug) {
+		$data =  array(
+					$this->id				=>	$bug->id,
+					$this->name				=>	$bug->name,
+					$this->description		=> 	$bug->description,
+					$this->category			=>	$bug->category,
+					$this->priority			=>	$bug->priority,
+					$this->status			=>	$bug->status,
+					$this->created			=>	$bug->created,
+					$this->createdBy		=>	$bug->createdBy,
+					$this->changed			=>	$bug->changed,
+					$this->changedBy		=>	$bug->changedBy,
+					$this->assignedTo		=>	$bug->assignedTo,
+					$this->operatingSystem	=>	$bug->operatingSystem,
+					$this->summery			=>	$bug->summery
+				);
+		
+		$id = (int)$bug->id;
+		if($id == 0){
+			$this->tableGateway->insert($data);
+		}else{
+			if($this->getBug($id)){
+				$this->tableGateway->update($data, array('id' => $id));
+			}else{
+				throw new Exception('Form id does not exist');
+			}
+		}
+	}
+	
+	public function delete($id){
+		$this->tableGateway->delete(array('id' => $id));
+	}
+}
