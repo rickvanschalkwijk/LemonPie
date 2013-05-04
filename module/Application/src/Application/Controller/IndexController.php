77bcaa5b7f2a48cbab6af65a6df810519826e2ef
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-05-04 17:06:15 +0200

    created project module

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -11,6 +11,8 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Bug\Model\BugTable;
+
 
 class IndexController extends AbstractActionController{
 	
